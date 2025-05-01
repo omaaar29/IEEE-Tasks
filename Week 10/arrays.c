7938597175d2512d507a4ec3be03c7e486c2c3fb
Omar <omarmohamed9823@gmail.com>
2025-05-01 18:54:45 +0300

    Update arrays.c

--- Week 10/arrays.c
+++ Week 10/arrays.c
@@ -115,3 +115,57 @@ int main()
 }
 
 //problem 9
+#include<stdio.h>
+
+int main()
+{
+    int size,x=0;
+    printf("Enter the size of the array: ");
+    scanf("%d",&size);
+
+   
+    int arr[size];
+    for(int i=0;i<size;i++)
+    {
+        printf("Enter the %d element of the array: ",++x);
+        scanf("%d",&arr[i]);
+    }
+
+    int arr2 [++size];
+    int num_position,num;
+    printf("Enter the position you want to enter your num in: ");
+    scanf("%d", &num_position);
+
+    if(num_position>size || num_position <= 0)
+    {
+        printf("invaild input-number");
+        return 0;
+    }
+    printf("Enter the num you want to enter: ");
+    scanf("%d",&num);
+
+
+    for(int i=0;i<(num_position-1);i++)
+    {
+        arr2[i]= arr[i]; 
+        x=i;
+    }
+
+    arr2[num_position-1]=num;
+
+    for(int i=++x;i<size;i++)
+    {
+        arr2[num_position]=arr[i];
+        num_position++;
+    }
+
+
+    printf("your new arr: ");
+    for(int i=0;i<(size);i++)
+    {
+        printf("%d ",arr2[i]);
+    }
+
+}
+
+//problem 13
